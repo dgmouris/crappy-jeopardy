c8f4a4952ac9f19077c2e53c5f291f747b89447a
Daniel Mouris <dgmouris@gmail.com>
2020-04-14 17:52:35 -0600

    Secore websocket change

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -2,7 +2,7 @@ const PRODUCTION = false;
 
 const SERVER_NAME = `www.nait.dev`;
 const SERVER_PORT = ``
-const WS_PROTOCOL = `ws://`
+const WS_PROTOCOL = `wss://`
 const WS_BASE_URL = `${WS_PROTOCOL}${SERVER_NAME}${SERVER_PORT}/ws`;
 const BASE_URL = `https://${SERVER_NAME}${SERVER_PORT}`;
 
