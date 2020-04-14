2a6d7128ae99e49cf7a647a2d72b65be89067d4a
Daniel Mouris <dgmouris@gmail.com>
2020-04-13 22:08:48 -0600

    Added server name to point to

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,6 +1,8 @@
 const PRODUCTION = false;
 
-const WS_BASE_URL = "ws://127.0.0.1:8000/ws";
-const BASE_URL = "http://localhost:8000";
+const SERVER_NAME = `204.209.76.194`;
+
+const WS_BASE_URL = `ws://${SERVER_NAME}:8000/ws`;
+const BASE_URL = `http://${SERVER_NAME}:8000`;
 
 export {BASE_URL, WS_BASE_URL};
