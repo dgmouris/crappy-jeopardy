8f8377cb9da29e90e4a9e8d6819da65a7a214027
Daniel Mouris <dgmouris@gmail.com>
2020-04-15 12:12:59 -0600

    Added for the talk

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,9 +1,10 @@
 const PRODUCTION = false;
 
-const SERVER_NAME = `www.nait.dev`;
-const SERVER_PORT = ``
-const WS_PROTOCOL = `wss://`
+const SERVER_NAME = `204.209.76.194`;
+const SERVER_PORT = `:8000`;
+const SERVER_PROTOCOL = "http://";
+const WS_PROTOCOL = `ws://`;
 const WS_BASE_URL = `${WS_PROTOCOL}${SERVER_NAME}${SERVER_PORT}/ws`;
-const BASE_URL = `https://${SERVER_NAME}${SERVER_PORT}`;
+const BASE_URL = `${SERVER_PROTOCOL}${SERVER_NAME}${SERVER_PORT}`;
 
 export {BASE_URL, WS_BASE_URL};
