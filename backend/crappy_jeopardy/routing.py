9908b8e93d8603e92be40bdb8d95fb0353cda688
Daniel Mouris <dgmouris@gmail.com>
2020-04-15 18:13:23 -0600

    updated routing

--- backend/crappy_jeopardy/routing.py
+++ backend/crappy_jeopardy/routing.py
@@ -1,12 +1,10 @@
 from channels.routing import ProtocolTypeRouter, URLRouter, ChannelNameRouter
-from channels.auth import AuthMiddlewareStack
 from channels.sessions import SessionMiddlewareStack
 
 from apps.questions.consumers import QuestionConsumer
 import apps.questions.routing
 
 application = ProtocolTypeRouter({
-    # 'websocket': AuthMiddlewareStack(
     'websocket': SessionMiddlewareStack(
         URLRouter(
             apps.questions.routing.websocket_urlpatterns
